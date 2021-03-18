8b9f3c4214a9b71f96027e2907f4fe940ab98a1e
coffin-sys <80905998+coffin-sys@users.noreply.github.com>
2021-03-18 17:07:57 +0200

    Update and rename jprq.go to cfc.go

--- jprq/cfc.go
+++ jprq/cfc.go
@@ -1,12 +1,12 @@
-package jprq
+package cfc
 
-type Jprq struct {
+type Cfc struct {
 	baseHost string
 	tunnels map[string]*Tunnel
 }
 
-func New(baseHost string) Jprq {
-	return Jprq{
+func New(baseHost string) Cfc {
+	return Cfc{
 		baseHost: baseHost,
 		tunnels: make(map[string]*Tunnel),
 	}
